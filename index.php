6b9c69b94cf48c9bcbecde24bfe48a94ee090ac5
GOVERNET\mmcguff <mmcguff@governet.net>
2017-09-11 07:22:29 -0600

    link to my random quote generator.

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 echo "Hello World!";
+
+echo <p>Made random quote generator that I deployed in Heroku.  Enjoy!</p>;
+echo  <a href="https://hidden-scrubland-99571.herokuapp.com/">Random Quote Generator</a>;
+echo <i><p>Give my random quote generator time to spin up. I learned it hybernates when not active for 30 mins</p></i>;
+
+
 ?>
